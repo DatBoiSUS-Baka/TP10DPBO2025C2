aef41dd60a51c87cbc061ea6e8c3ea72130fac0f
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-05-22 17:57:39 +0700

    Membuat Anime.php dan membuat method getAll, create, dan update

---
+++ Kode_Program/model/Anime.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'config/Database.php';
+
+class Anime {
+    private $conn;
+    private $table = 'anime';
+
+    public function __construct(){
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function getAll(){
+        $query = "SELECT * FROM " . $this->table;
+        $stmt = $this->conn->prepare($query);
+        $stms->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create($name, $genre){
+        $query = "INSERT INTO " . $this->table . " (nama_anime, genre_anime) VALUES (:name, :genre)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':genre', $genre);
+        return $stms->execute();
+    }
+
+    public function update($id, $name, $genre){
+        $query = "UPDATE " . $this->table . " SET nama_anime = :name, genre_anime = :genre WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':genre', $genre);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
+}
