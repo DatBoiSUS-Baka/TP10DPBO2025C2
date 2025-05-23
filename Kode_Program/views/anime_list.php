4a6983eeb0e5bca57d609531ad84240e39430524
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-05-23 16:56:40 +0700

    Membuat anime_list.php

---
+++ Kode_Program/views/anime_list.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'views/template/header.php';
+?>
+
+<h2 class="text-xl mb-4">Anime List</h2>
+<a href="index.php?entity=anime&action=add" class="bg-green-500 text-white px-4 py-2 rounded mb-4 inline-block">Add Anime</a>
