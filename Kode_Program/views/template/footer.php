8b2f1e80c7eb4d7598dae629445574980660634f
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-05-23 16:52:40 +0700

    Membuat template untuk view

---
+++ Kode_Program/views/template/footer.php
@@ -0,0 +1,6 @@
+</div>
+    <footer class="bg-gray-800 text-white p4 text-center">
+        <p>$copy; 2025 List Anime Collection</p>
+    </footer>
+</body>
+</html>
---
+++ Kode_Program/views/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Anime List Collection</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+    <nav class="bg-green-500 p4 text-white">
+        <h1 class="text-2xl">Anime List Collection</h1>
+        <ul class="flex space-x-4 mt-2">
+            <li><a href="index.php?entity=anime" class="hover:underline">Anime</a></li>
+            <li><a href="index.php?entity=user" class="hover:underline">Users</a></li>
+            <li><a href="index.php?entity=listAnime" class="hover:underline">List Anime</a></li>
+
+        </ul>
+    </nav>
+    <div class="container mx-auto p-4">
